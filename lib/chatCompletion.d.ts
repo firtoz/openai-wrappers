888a84ed7b74aef2a2ec97be041199cbe7686aa1
firtoz <108406948+firtoz@users.noreply.github.com>
2023-03-04 13:51:42 +0000

    Remove ignored files

--- lib/chatCompletion.d.ts
+++
@@ -1,4 +0,0 @@
-import { ChatCompletionRequestMessage, OpenAIApi } from "openai";
-import { ChatCompletionOptions, ChatStreamDelta, CustomCompletionError } from "./types";
-export declare function getChatCompletionAdvanced(openai: OpenAIApi, messages: ChatCompletionRequestMessage[], options: Partial<ChatCompletionOptions> | undefined, onProgress: (result: ChatStreamDelta) => void, onError: (error: CustomCompletionError) => void): Promise<void>;
-export declare function getChatCompletionSimple(openai: OpenAIApi, messages: ChatCompletionRequestMessage[], options?: Exclude<Partial<ChatCompletionOptions>, 'stream'>): Promise<string>;
--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { CompletionError, CompletionErrorType, CompletionParams, CustomCompletionError, ChatCompletionOptions, ChatModelName, ModelName, ChatStreamDelta, ChatStreamDeltaChoice, } from './types';
-export { getCompletionSimple, getCompletionAdvanced, } from './completion';
-export { getChatCompletionSimple, getChatCompletionAdvanced, } from './chatCompletion';
