ba1a4db0f0a9c5c1a1917c38dec7ef8ac99cf80a
firtoz <108406948+firtoz@users.noreply.github.com>
2023-03-04 13:48:24 +0000

    Update build

--- lib/chatCompletion.d.ts
+++ lib/chatCompletion.d.ts
@@ -1,4 +1,4 @@
 import { ChatCompletionRequestMessage, OpenAIApi } from "openai";
 import { ChatCompletionOptions, ChatStreamDelta, CustomCompletionError } from "./types";
+export declare function getChatCompletionAdvanced(openai: OpenAIApi, messages: ChatCompletionRequestMessage[], options: Partial<ChatCompletionOptions> | undefined, onProgress: (result: ChatStreamDelta) => void, onError: (error: CustomCompletionError) => void): Promise<void>;
 export declare function getChatCompletionSimple(openai: OpenAIApi, messages: ChatCompletionRequestMessage[], options?: Exclude<Partial<ChatCompletionOptions>, 'stream'>): Promise<string>;
-export declare function getChatCompletion(openai: OpenAIApi, messages: ChatCompletionRequestMessage[], options: Partial<ChatCompletionOptions> | undefined, onProgress: (result: ChatStreamDelta) => void, onError: (error: CustomCompletionError) => void): Promise<void>;
